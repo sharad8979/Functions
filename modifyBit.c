55452471c48e639e221b6b0606a1b2f5ff7c63e9
sharad8979 <sharadsharma8439@gmail.com>
2024-11-07 14:39:31 +0530

    Add files via upload

--- modifyBit.c
+++ modifyBit.c
@@ -1,23 +1,19 @@
-
-#include <stdio.h>
-int modifyBit(int ,int);
-int main() {
-    int n,p,res;
-    printf("Enter the number=");
-    scanf("%d",&n);
-    printf("Enter the bit position which you want to modified=");
-    scanf("%d",&p);
-    res = modifyBit(n,p);
-     printf("\nOriginal number = %d",n);
-     printf("\nNumber after modification = %d",res);
-    return 0;
+#include<stdio.h>
+int modifyBit(int , int);
+int main(){
+	int n,p,res;
+	printf("Enter the number=");
+	scanf("%d",&n);
+	printf("\nEnter the position bit(index from 0) that you want to be modified=");
+	scanf("%d",&p);
+	res=modifyBit(n,p);
+	printf("\nOriginal number = %d",n);
+	printf("\nNumber after modification=%d",res);
+	return 0;
 }
 
-int modifyBit(int n , int p){
-    int r;
-    r = 1<<p;
-    return n^r;
+int modifyBit(int n,int p){
+	int r;
+	r=1<<p;
+	return n^p;
 }
-
-
-
